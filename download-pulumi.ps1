e6ee92e4a3aaf7f8d72545a342bb50eb5e17e33d
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2024-03-17 17:56:19 -0500

    Add user-agent to download script

--- download-pulumi.ps1
+++ download-pulumi.ps1
@@ -1,5 +1,5 @@
 param (
-  [string]$version = "latest",
+  [string]$version = "v3.111.0",
   [string]$arch = "x64",
   [bool]$clean = $false,
   [string]$pat
@@ -12,12 +12,12 @@ $url = "https://api.github.com/repos/pulumi/pulumi/releases/tags/$version"
 $headers = @{
   'Accept' = 'application/vnd.github+json';
   'Authorization' = "Bearer $pat";
-  'X-GitHub-Api-Version' = '2022-11-28'
+  'X-GitHub-Api-Version' = '2022-11-28';
+  'User-Agent' = 'PulumiBot/1.0'
 }
 
 # Use Invoke-RestMethod to send a GET request to the URL
 $response = Invoke-RestMethod -Uri $url -Headers $headers
-# $response.assets | Where-Object { $_.name -like "*-windows-$arch.zip" } | Select-Object -ExpandProperty name | Format-Table | Out-Host
 
 # Extract the download URL for the zip file from the response
 $zipUrl = $response.assets | Where-Object { $_.name -like "*-windows-$arch.zip" } | Select-Object -First 1 -ExpandProperty browser_download_url
@@ -37,7 +37,7 @@ if (Test-Path -Path $zipFile -PathType Leaf) {
 # Define the path where the zip file will be extracted
 $extractPath = "./packages/$arch"
 
-# Create the directory if it doesn't exist
+# Create the directory if it doesn't exist, clean up if we need to
 if (!(Test-Path -Path $extractPath)) {
   Write-Host "Setting up target path $extractPath"
   New-Item -ItemType Directory -Path $extractPath | Out-Null
@@ -48,7 +48,9 @@ elseif ($clean) {
   New-Item -ItemType Directory -Path $extractPath | Out-Null
 }
 
-# Extract the zip file
+# Extract the zip file, but move all the contents out of ./pulumi/bin to ./
 Expand-Archive -Path $zipFile -DestinationPath $extractPath -Force
+Get-ChildItem -Path $extractPath/pulumi/bin/* -Recurse | Move-Item -Destination $extractPath -Force
+Remove-Item -Path $extractPath/pulumi -Recurse -Force | Out-Null
 
 Write-Host "Pulumi version $version has been downloaded and unzipped to $extractPath"
